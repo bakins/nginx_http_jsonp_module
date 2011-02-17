b0d76dd4bfa36d8604c5a63cc59b9527a3629d49
Brian Akins <brian@akins.org>
2011-02-17 08:39:37 -0500

    set default type based on how gzip does it

--- src/ngx_http_jsonp_module.c
+++ src/ngx_http_jsonp_module.c
@@ -149,8 +149,8 @@ static char * ngx_http_jsonp_merge_conf(ngx_conf_t *cf, void *parent, void *chil
 
 
     // Merge the applicable mimetypes
-    if (ngx_http_merge_types(cf, conf->mimetypes_keys, &conf->mimetypes,
-                             prev->mimetypes_keys, &prev->mimetypes,
+    if (ngx_http_merge_types(cf, &conf->mimetypes_keys, &conf->mimetypes,
+                             &prev->mimetypes_keys, &prev->mimetypes,
                              ngx_http_jsonp_default_mimetypes) != NGX_OK)
     {
         return NGX_CONF_ERROR;
